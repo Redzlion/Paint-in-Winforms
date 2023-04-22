c7830b6b910e80dacd853bc361cf3ff27585bfe7
Yusif Alakbarzade <73228864+Redzlion@users.noreply.github.com>
2023-04-22 15:11:29 +0200

    Add files via upload

---
+++ Paint winforms/winforml1/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿
+namespace winforml1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.FileToolStripLabel = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripButtonSave = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonLoad = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.ToolStripLabel = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonRect = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonEllipse = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonTrash = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.ThicknessToolStripLabel = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripComboBoxThick = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.ChosenColorToolStripLabel = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripButtonChosenColor = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.LangaugeToolStripLabel = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripButtonEN = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonPL = new System.Windows.Forms.ToolStripButton();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.ColorsGroupBox = new System.Windows.Forms.GroupBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.Pbox = new System.Windows.Forms.PictureBox();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.toolStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.ColorsGroupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Pbox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FileToolStripLabel,
+            this.toolStripButtonSave,
+            this.toolStripButtonLoad,
+            this.toolStripSeparator1,
+            this.ToolStripLabel,
+            this.toolStripButton1,
+            this.toolStripButtonRect,
+            this.toolStripButtonEllipse,
+            this.toolStripButtonTrash,
+            this.toolStripSeparator2,
+            this.ThicknessToolStripLabel,
+            this.toolStripComboBoxThick,
+            this.toolStripSeparator3,
+            this.ChosenColorToolStripLabel,
+            this.toolStripButtonChosenColor,
+            this.toolStripSeparator4,
+            this.LangaugeToolStripLabel,
+            this.toolStripButtonEN,
+            this.toolStripButtonPL});
+            resources.ApplyResources(this.toolStrip1, "toolStrip1");
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.toolStrip1_ItemClicked);
+            // 
+            // FileToolStripLabel
+            // 
+            this.FileToolStripLabel.Name = "FileToolStripLabel";
+            resources.ApplyResources(this.FileToolStripLabel, "FileToolStripLabel");
+            // 
+            // toolStripButtonSave
+            // 
+            this.toolStripButtonSave.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            resources.ApplyResources(this.toolStripButtonSave, "toolStripButtonSave");
+            this.toolStripButtonSave.Name = "toolStripButtonSave";
+            this.toolStripButtonSave.Click += new System.EventHandler(this.Save);
+            // 
+            // toolStripButtonLoad
+            // 
+            this.toolStripButtonLoad.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            resources.ApplyResources(this.toolStripButtonLoad, "toolStripButtonLoad");
+            this.toolStripButtonLoad.Name = "toolStripButtonLoad";
+            this.toolStripButtonLoad.Click += new System.EventHandler(this.Loads);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            resources.ApplyResources(this.toolStripSeparator1, "toolStripSeparator1");
+            // 
+            // ToolStripLabel
+            // 
+            this.ToolStripLabel.Name = "ToolStripLabel";
+            resources.ApplyResources(this.ToolStripLabel, "ToolStripLabel");
+            this.ToolStripLabel.Click += new System.EventHandler(this.toolStripLabelTools_Click);
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.BackColor = System.Drawing.SystemColors.Control;
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.ForeColor = System.Drawing.Color.Black;
+            resources.ApplyResources(this.toolStripButton1, "toolStripButton1");
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButtonRect
+            // 
+            this.toolStripButtonRect.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            resources.ApplyResources(this.toolStripButtonRect, "toolStripButtonRect");
+            this.toolStripButtonRect.Name = "toolStripButtonRect";
+            this.toolStripButtonRect.Click += new System.EventHandler(this.toolStripButtonRect_Click);
+            // 
+            // toolStripButtonEllipse
+            // 
+            this.toolStripButtonEllipse.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            resources.ApplyResources(this.toolStripButtonEllipse, "toolStripButtonEllipse");
+            this.toolStripButtonEllipse.Name = "toolStripButtonEllipse";
+            this.toolStripButtonEllipse.Click += new System.EventHandler(this.toolStripButtonEllipse_Click);
+            // 
+            // toolStripButtonTrash
+            // 
+            this.toolStripButtonTrash.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            resources.ApplyResources(this.toolStripButtonTrash, "toolStripButtonTrash");
+            this.toolStripButtonTrash.Name = "toolStripButtonTrash";
+            this.toolStripButtonTrash.Click += new System.EventHandler(this.Trash_C);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            resources.ApplyResources(this.toolStripSeparator2, "toolStripSeparator2");
+            // 
+            // ThicknessToolStripLabel
+            // 
+            this.ThicknessToolStripLabel.Name = "ThicknessToolStripLabel";
+            resources.ApplyResources(this.ThicknessToolStripLabel, "ThicknessToolStripLabel");
+            // 
+            // toolStripComboBoxThick
+            // 
+            this.toolStripComboBoxThick.Name = "toolStripComboBoxThick";
+            resources.ApplyResources(this.toolStripComboBoxThick, "toolStripComboBoxThick");
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            resources.ApplyResources(this.toolStripSeparator3, "toolStripSeparator3");
+            // 
+            // ChosenColorToolStripLabel
+            // 
+            this.ChosenColorToolStripLabel.Name = "ChosenColorToolStripLabel";
+            resources.ApplyResources(this.ChosenColorToolStripLabel, "ChosenColorToolStripLabel");
+            // 
+            // toolStripButtonChosenColor
+            // 
+            this.toolStripButtonChosenColor.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            resources.ApplyResources(this.toolStripButtonChosenColor, "toolStripButtonChosenColor");
+            this.toolStripButtonChosenColor.Name = "toolStripButtonChosenColor";
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            resources.ApplyResources(this.toolStripSeparator4, "toolStripSeparator4");
+            // 
+            // LangaugeToolStripLabel
+            // 
+            this.LangaugeToolStripLabel.Name = "LangaugeToolStripLabel";
+            resources.ApplyResources(this.LangaugeToolStripLabel, "LangaugeToolStripLabel");
+            // 
+            // toolStripButtonEN
+            // 
+            this.toolStripButtonEN.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            resources.ApplyResources(this.toolStripButtonEN, "toolStripButtonEN");
+            this.toolStripButtonEN.Name = "toolStripButtonEN";
+            this.toolStripButtonEN.Click += new System.EventHandler(this.toolStripButtonEN_Click);
+            // 
+            // toolStripButtonPL
+            // 
+            this.toolStripButtonPL.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            resources.ApplyResources(this.toolStripButtonPL, "toolStripButtonPL");
+            this.toolStripButtonPL.Name = "toolStripButtonPL";
+            this.toolStripButtonPL.Click += new System.EventHandler(this.toolStripButtonPL_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
+            this.tableLayoutPanel1.Controls.Add(this.ColorsGroupBox, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.Pbox, 0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            // 
+            // ColorsGroupBox
+            // 
+            this.ColorsGroupBox.Controls.Add(this.flowLayoutPanel1);
+            resources.ApplyResources(this.ColorsGroupBox, "ColorsGroupBox");
+            this.ColorsGroupBox.Name = "ColorsGroupBox";
+            this.ColorsGroupBox.TabStop = false;
+            this.ColorsGroupBox.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // flowLayoutPanel1
+            // 
+            resources.ApplyResources(this.flowLayoutPanel1, "flowLayoutPanel1");
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            // 
+            // Pbox
+            // 
+            resources.ApplyResources(this.Pbox, "Pbox");
+            this.Pbox.Name = "Pbox";
+            this.Pbox.TabStop = false;
+            this.Pbox.Click += new System.EventHandler(this.Pbox_Click);
+            this.Pbox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Pbox_MouseDown);
+            this.Pbox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Pbox_MouseMove);
+            this.Pbox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Pbox_MouseUp_1);
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.toolStrip1);
+            this.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ColorsGroupBox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Pbox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox Pbox;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.GroupBox ColorsGroupBox;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.ToolStripLabel FileToolStripLabel;
+        private System.Windows.Forms.ToolStripButton toolStripButtonSave;
+        private System.Windows.Forms.ToolStripButton toolStripButtonLoad;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripLabel ToolStripLabel;
+        private System.Windows.Forms.ToolStripButton toolStripButtonBrush;
+        private System.Windows.Forms.ToolStripButton toolStripButtonRect;
+        private System.Windows.Forms.ToolStripButton toolStripButtonEllipse;
+        private System.Windows.Forms.ToolStripButton toolStripButtonTrash;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripLabel ThicknessToolStripLabel;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBoxThick;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripLabel ChosenColorToolStripLabel;
+        private System.Windows.Forms.ToolStripButton toolStripButtonChosenColor;
+        private System.Windows.Forms.ToolStripLabel LangaugeToolStripLabel;
+        private System.Windows.Forms.ToolStripButton toolStripButtonEN;
+        private System.Windows.Forms.ToolStripButton toolStripButtonPL;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+    }
+}
+
---
+++ Paint winforms/winforml1/Form1.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+using System.Resources;
+using System.Threading;
+using System.Reflection;
+
+namespace winforml1
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap drawArea;
+        private static Pen pen_;
+        public bool change_;
+        public bool changeEllipse_;
+        public bool changeRect_;
+        public bool draw;
+        public bool drawSquare;
+        public bool drawEllipse;
+        public const int r = 2;
+        public int x, y, sX, sY, cX, cY;
+        public int index;
+        Pen p = new Pen(Color.Black, 1);
+
+        //public Form1(CultureInfo culture)
+        //{
+        //    InitializeComponent();
+
+        //    Thread.CurrentThread.CurrentCulture = culture;
+        //    Thread.CurrentThread.CurrentUICulture = culture;
+
+        //    ResourceManager rm = new ResourceManager("winforml1.Form1", System.Reflection.Assembly.GetExecutingAssembly());
+
+        //    this.Text = this.Text + " " + DateTime.Now.ToLongDateString();
+            
+        //    toolStripLabelFile.Text = rm.GetString("toolStripLabelFile");
+        //    toolStripLabelTools.Text = rm.GetString("toolStripLabelTools");
+        //    toolStripLabelThick.Text = rm.GetString("toolStripLabelThick");
+        //    toolStripLabelChosenColor.Text = rm.GetString("toolStripLabelChosenColor");
+        //    toolStripLabelLanguage.Text = rm.GetString("toolStripLabelLanguage");
+        //    groupBox1.Text = rm.GetString("groupBox1");
+        //}
+
+        public Form1()
+        {
+            InitializeComponent();        
+            drawArea = new Bitmap(Pbox.Size.Width, Pbox.Size.Height);
+            Pbox.Image = drawArea;
+            using (Graphics g = Graphics.FromImage(drawArea))
+            {
+                g.Clear(Color.White);
+            }
+
+            pen_ = new Pen(Brushes.Black, 3);
+            toolStripButtonChosenColor.BackColor = Color.Black;
+
+            toolStripComboBoxThick.Items.Add("1");
+            toolStripComboBoxThick.Items.Add("2");
+            toolStripComboBoxThick.Items.Add("3");
+            toolStripComboBoxThick.Text = "1";
+
+            //int up_ , left_ = 0;
+            //int Count = 20;
+            //PictureBox[] pbs = new PictureBox[Count];
+            //for (int i = 0; i < Count; i++)
+            //{
+            //    pbs[i] = new PictureBox();
+            //    pbs[i].Top = up_;
+            //    pbs[i].Left = left_;
+            //    pbs[i].Size = new Size(25, 25);
+            //    pbs[i].BackColor = 
+            //    pbs[i].Click += new EventHandler(DrawArea);
+            //}
+
+
+            foreach (KnownColor color in Enum.GetValues(typeof(KnownColor)))
+            {
+                Button button = new Button();
+                button.BackColor = Color.FromKnownColor(color);
+                button.Size = new Size(25, 25);
+                button.Name = color.ToString();
+                button.Click += new EventHandler(btnC_Event);
+                flowLayoutPanel1.Controls.Add(button);
+            }
+        }
+
+        private void Save(object sender, EventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            save.DefaultExt = "png";
+            save.Filter = "Png Image| *.png | Bitmap Image | *.bmp | Jpeg Image | *.jpg"; ;  //"Image Files(*.BMP;*.JPG;*.GIF)|*.BMP;*.JPG;*.GIF";
+            if (save.ShowDialog() == DialogResult.OK)
+            { 
+                drawArea.Save(save.FileName, System.Drawing.Imaging.ImageFormat.Png);
+            }
+        }
+
+        private void Loads(object sender, EventArgs e)
+        {
+            //    OpenFileDialog fileDialog = new OpenFileDialog();
+            //    fileDialog.DefaultExt = "jpg";
+            //    fileDialog.Filter = "Image Files(*.BMP; *.JPG; *.GIF)| *.BMP; *.JPG; *.GIF";
+            //    if (fileDialog.ShowDialog() == DialogResult.OK)
+            //    {
+            //        tableLayoutPanel1.GetControlFromPosition(0, 0).BackgroundImage =
+            //        new Bitmap(fileDialog.FileName);
+            //        tableLayoutPanel1.GetControlFromPosition(0, 0).BackgroundImageLayout =
+            //        ImageLayout.Stretch;
+            //    }
+            String imageLocation = "";
+    
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Filter = "jpg files(*.jpg)|*.jpg| PNG files(*.png)|*.png| All Files(*.*)|*.*";
+
+                if(dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    imageLocation = dialog.FileName;
+                    Pbox.ImageLocation = imageLocation;
+                }  
+        }
+
+        private void Trash_C(object sender, EventArgs e)
+        {
+            using (Graphics g = Graphics.FromImage(drawArea))
+            {
+                g.Clear(Color.White);
+                Pbox.Refresh();
+
+            }
+        }
+
+        public void DrawPoint(int x, int y, Color color)
+        {
+            Graphics g = this.Pbox.CreateGraphics();
+            Pen pen = new Pen(color);
+            g.DrawRectangle(pen, x, y, 3, 3);
+        }
+
+     
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            change_ = !change_;
+            toolStripButton1.Checked = change_;
+            index = 1;
+        }
+
+
+
+       
+        private void Pbox_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+                draw = true;
+
+            cX = e.X;
+            cY = e.Y;
+
+        }
+        private void Pbox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (draw && change_ && index == 1)
+            {         
+                using (Graphics g = Graphics.FromImage(drawArea))
+                {
+                    g.FillEllipse(Brushes.Black, e.X - r, e.Y - r, r * 3, r * 3);
+
+                }
+                Pbox.Refresh();
+            }
+            Pbox.Refresh();
+            x = e.X;
+            y = e.Y;
+            sX = e.X - cX;
+            sY = e.Y - cY;
+            
+        }
+        private void Pbox_MouseUp_1(object sender, MouseEventArgs e)
+        {
+            draw = false;
+
+            sX = x - cX;
+            sY = y - cY;
+
+            if (index == 2 )
+            {
+                
+                using (Graphics g = Graphics.FromImage(drawArea))
+                {
+                    g.DrawEllipse(p, cX, cY, sX, sY);
+                }
+               
+            }
+            if(index == 3)
+            {
+                using (Graphics g = Graphics.FromImage(drawArea))
+                {
+                    Pen blackPen = new Pen(Color.Black, 5);
+                    g.DrawRectangle(p, cX, cY, sX, sY);
+                }
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+         
+        }
+
+        private void toolStripButtonEllipse_Click(object sender, EventArgs e)
+        {
+            index = 2;
+
+            changeEllipse_ = !changeEllipse_;
+            toolStripButtonEllipse.Checked = changeEllipse_;
+           
+
+        }
+
+        private void toolStripButtonRect_Click(object sender, EventArgs e)
+        {
+            index = 3;
+            changeRect_ = !changeRect_;
+            toolStripButtonRect.Checked = changeRect_;
+         
+        }
+
+        private void toolStripButtonPL_Click(object sender, EventArgs e)
+        {
+
+            //Form1 f = new Form1(new CultureInfo("pl-PL"));
+            //f.ShowDialog();
+            //Thread.CurrentThread.CurrentCulture = new(("pl-PL"));
+            //Thread.CurrentThread.CurrentUICulture = new(("pl-PL"));
+
+            ResourceManager rm = new ResourceManager("winforml1.Properties.Resources", Assembly.GetExecutingAssembly());
+            FileToolStripLabel.Text = rm.GetString("Files");
+            ToolStripLabel.Text = rm.GetString("Tools");
+            ThicknessToolStripLabel.Text = rm.GetString("Thickness");
+            ChosenColorToolStripLabel.Text = rm.GetString("Chosen Color");
+            LangaugeToolStripLabel.Text = rm.GetString("Langauge");
+            ColorsGroupBox.Text = rm.GetString("Colors");
+        }
+
+        private void toolStripButtonEN_Click(object sender, EventArgs e)
+        {
+            //Form1 f = new Form1(new CultureInfo("en-US"));
+            //f.ShowDialog();
+            //Thread.CurrentThread.CurrentCulture = new(("en-EN"));
+            //Thread.CurrentThread.CurrentUICulture = new(("en-EN"));
+
+            ResourceManager rm = new ResourceManager("winforml1.Properties.en_local", Assembly.GetExecutingAssembly());
+            FileToolStripLabel.Text = rm.GetString("Akta");
+            ToolStripLabel.Text = rm.GetString("Narzędzia");
+            ThicknessToolStripLabel.Text = rm.GetString("grubość");
+            ChosenColorToolStripLabel.Text = rm.GetString("Wybrany kolor");
+            LangaugeToolStripLabel.Text = rm.GetString("Język");
+            ColorsGroupBox.Text = rm.GetString("Zabarwienie");
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+        
+        }
+
+        private void toolStripLabelTools_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Pbox_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void btnC_Event(object sender, EventArgs e)
+        {
+            toolStripButtonChosenColor.BackColor = pen_.Color = ((Button)sender).BackColor;
+        }
+    }
+}
